5abbb6231c15bdd1bb73161b588e2dac773896de
Henry Bronx <klimovich.maks@gmail.com>
2024-01-20 18:04:26 +0300

    add Models

---
+++ Models/StorageEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppGrapQL.Models
+{
+    public class StorageEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual List<ProductEntity> Products { get; set; }
+    }
+}
