b884144a8a3832d57bfb3b9bdfc8894317a2e0b9
Henry Bronx <klimovich.maks@gmail.com>
2024-01-20 18:02:53 +0300

    add Querty

---
+++ Query/MySimpleQuery.cs
@@ -0,0 +1,13 @@
+﻿using WebAppGrapQL.Abstractions;
+using WebAppGrapQL.Models;
+using WebAppGrapQL.Models.Dto;
+
+namespace WebAppGrapQL.Query
+{
+    public class MySimpleQuery
+    {
+        public IEnumerable<ProductDto> GetProducts([Service] IProductService service) => service.GetProducts();
+        public IEnumerable<StorageDto> GetStorages([Service] IStorageService service) => service.GetStorages();
+        public IEnumerable<CategoryDto> GetCategories([Service] ICategoryService service) => service.GetCategories();
+    }
+}
---
+++ Query/WarehouseItemQuery.cs
@@ -0,0 +1,13 @@
+﻿using WebAppGrapQL.Abstractions;
+using WebAppGrapQL.Models.Dto;
+
+namespace WebAppGrapQL.Query
+{
+    public class WarehouseItemQuery
+    {
+        public List<WarehouseItem> GetItems(int warehouseId, [Service] IWarehouseService warehouseService)
+        {
+            return warehouseService.GetItems(warehouseId);
+        }
+    }
+}
