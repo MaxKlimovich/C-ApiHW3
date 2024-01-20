f1e550d59c3b37e2c573352743687eeb94a6e2f1
Henry Bronx <klimovich.maks@gmail.com>
2024-01-20 18:05:00 +0300

    add Mapper

---
+++ Mapper/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WebAppGrapQL.Models;
+using WebAppGrapQL.Models.Dto;
+
+namespace WebAppGrapQL.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<ProductEntity, ProductDto>().ReverseMap();
+            CreateMap<StorageEntity, StorageDto>().ReverseMap();
+            CreateMap<CategoryEntity, CategoryDto>().ReverseMap();
+        }
+    }
+}
